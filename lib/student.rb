7eea85453153bf5c8ed8b24a4f61118ebe079ab3
Daniel Abraham Braude <daniboy1007@gmail.com>
2021-01-04 06:23:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+require_relative "../lib/user.rb"
+class Student
+class Student < User
+  attr_accessor :knowledge
 
+end
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(string)
+    knowledge << string
+  end
 end
